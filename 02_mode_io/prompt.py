c3fe6ffa13340dee13c4fc23b1d64a66bdab6dea
shoakira <shojiguchi.akira@robocip.or.jp>
2025-03-06 18:59:23 +0900

    インポートパスを更新し、モデルを「gpt-3.5-turbo」から「gpt-4o」に変更、invokeメソッドを使用するように修正

--- 02_mode_io/prompt.py
+++ 02_mode_io/prompt.py
@@ -1,9 +1,9 @@
-from langchain import PromptTemplate  #← PromptTemplateをインポート
+from langchain_core.prompts import PromptTemplate  # 新しいインポートパス
 
-prompt = PromptTemplate(  #← PromptTemplateを初期化する
-    template="{product}はどこの会社が開発した製品ですか？",  #← {product}という変数を含むプロンプトを作成する
+prompt = PromptTemplate(  
+    template="{product}はどこの会社が開発した製品ですか？",  
     input_variables=[
-        "product"  #← productに入力する変数を指定する
+        "product"  
     ]
 )
 
--- 02_mode_io/prompt_and_language_model.py
+++ 02_mode_io/prompt_and_language_model.py
@@ -1,19 +1,19 @@
-from langchain import PromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 
-chat = ChatOpenAI(  #←クライアントを作成しchatへ保存
-    model="gpt-3.5-turbo",  #← 呼び出すモデルを指定
+chat = ChatOpenAI(
+    model="gpt-4o",
 )
 
-prompt = PromptTemplate(  #← PromptTemplateを作成する
-    template="{product}はどこの会社が開発した製品ですか？",  #← {product}という変数を含むプロンプトを作成する
+prompt = PromptTemplate(
+    template="{product}はどこの会社が開発した製品ですか？",
     input_variables=[
-        "product"  #← productに入力する変数を指定する
+        "product"
     ]
 )
 
-result = chat( #← 実行する
+result = chat.invoke(  # invokeメソッドを使用
     [
         HumanMessage(content=prompt.format(product="iPhone")),
     ]
